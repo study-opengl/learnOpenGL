0cb6193d036aa288996ff37d2fe6ddc851b1df06
ashoka <savebtc11@gmail.com>
2019-03-27 01:19:39 +0800

    动态改变光照颜色

--- openglLearning/material/main.cpp
+++ openglLearning/material/main.cpp
@@ -242,8 +242,14 @@ void drawCube(ShaderProgram &cubeShader, unsigned int vao)
     cubeShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
     cubeShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
     cubeShader.setFloat("material.shininess", 32.0f);
-    cubeShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-    cubeShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+    glm::vec3 lightColor;
+    lightColor.x = sin(glfwGetTime() * 2.0f);
+    lightColor.y = sin(glfwGetTime() * 0.7f);
+    lightColor.z = sin(glfwGetTime() * 1.3f);
+    glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
+    glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
+    cubeShader.setVec3("light.ambient", ambientColor.x, ambientColor.y, ambientColor.z);
+    cubeShader.setVec3("light.diffuse", diffuseColor.x, diffuseColor.y, diffuseColor.z);
     cubeShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 }
